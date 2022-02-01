fa65a1593421fe1078074a15c9d0752a50d13760
Tejas Borse <borsetejas764@gmail.com>
2022-02-01 16:32:39 +0530

    PHP not connected

--- entry.js
+++ entry.js
@@ -1,14 +1,14 @@
 function Validate() {
-	var username = document.getElementById("usernames").value;
-	var password = document.getElementById("passwords").value;
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
 
 	if(username=="TEJAS" && password=="BORSE")
 	{
-		alert("LOGIN SUCCESSFULL");
+		//alert("LOGIN SUCCESSFULL");
 		return false;
 	}
 	else
 	{
-		alert("LOGIN FAILED");
+		//alert("LOGIN FAILED");
 	}
 }
