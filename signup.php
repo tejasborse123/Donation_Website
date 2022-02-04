ed34807cc0d4bfbcca8cfcc4e0c1207367a14525
Niket Sonawane <niketsonawane49@gmail.com>
2022-02-04 22:10:47 +0530

    pushing all

--- signup.php
+++ signup.php
@@ -1,11 +1,27 @@
 <?php
-    $email = $_POST['email'];
-    $fullname = $_POST['fullname'];
-    $username = $_POST['username'];
+
+   include 'otp.html';
+
+   $email = $_POST['email'];
+   $fullname = $_POST['fullname'];
+   $username = $_POST['username'];
 	$password = $_POST['password'];
 
-	echo "Email : " , $email ,"<br>";
-	echo "fullname : " ,$fullname ,"<br>";
-	echo "Username : " ,$username ,"<br>";
-	echo "Password : " ,$password ,"<br>";	
+   $code = rand(100000,999999);
+   
+   session_start();
+   $_SESSION['code'] = $code;
+	
+	$headers = "From: savelifes07@gmail.com";
+	$subject = "verify otp";
+	$message = " Hey $fullname here is your OTP for save life foundation, $code ";
+	if(mail($email, $subject, $message, $headers))
+	{
+		echo "Emai succesfully send to $email ";
+	}
+	else
+	{
+		echo "Email send failed";
+	}
+
 ?>
