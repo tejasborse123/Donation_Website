a97a8e76b7ffb7a918cd0a4f1288bde42c70e6d6
Tejas Borse <borsetejas764@gmail.com>
2022-02-06 22:32:06 +0530

    DataBase Connected

--- verify.php
+++ verify.php
@@ -14,4 +14,28 @@ else
 	include 'otp.html';
 }
 
+$conn = mysqli_connect("localhost", "root", "","savelife");
+		
+	$email = $_SESSION['email'];
+	$fullname = $_SESSION['fullname'];
+	$username = $_SESSION['username'];
+	$password = $_SESSION['password'];
+
+		if(!$conn){
+			die("Sorry Failed to connect : ". mysqli_connect_error());
+		}
+		else{
+			echo "connection successfull";
+			}
+			 
+		$sql = "INSERT INTO `users` (`email`, `fullname`, `username`, `password`) VALUES ('$email' , '$fullname' , '$username' , '$password')";
+		$result = mysqli_query($conn,$sql) or die(mysqli_error());
+			
+		if($result) {
+					echo "<br>Form Submitted Successfully";
+			}
+			else{
+					echo "<br>Form not Submitted because : ". mysqli_error($conn);
+			}
+
 ?>
