c978616120324490064334db0b5e52e2bc37a6c7
Tejas Borse <borsetejas764@gmail.com>
2022-02-07 10:52:46 +0530

    Login Failed messege Displayed

--- login.php
+++ login.php
@@ -1,6 +1,32 @@
 <?php
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	echo $username ,"<br>";
-	echo $password ,"<br>";	
+
+	$conn = mysqli_connect("localhost", "root", "","savelife") or die("Connection Failed");
+
+	if(!$conn){
+			die("Sorry Failed to connect : ". mysqli_connect_error());
+		}
+		else{
+			//echo "connection successfull <br>";
+		}
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		$query = "SELECT * FROM `usersinfo` WHERE `Email` LIKE '$username' AND `Password` LIKE '$password'";
+
+		$result = mysqli_query($conn,$query) or die(mysqli_error());
+
+		$count = mysqli_num_rows($result);
+
+		if ($count>0) {
+			echo "Login Successfull";
+		}
+
+		else{
+			$label = "Login Failed";
+			session_start();
+			$_SESSION['label']=$label;
+			include 'entry.html';
+		}
+	
 ?>
